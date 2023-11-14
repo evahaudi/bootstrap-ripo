a012dd95fa766893fed4031494830f5ce5923bfc
EVERLYNE AKINYI <evahaudi2354@gmail.com>
2023-11-14 15:03:16 +0300

    upadated bio

--- calculater.py
+++ calculater.py
@@ -2,8 +2,8 @@ import tkinter as tk
 
 def calculate():
     try:
-        num1 = int(entry1.get())
-        num2 = int(entry2.get())
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
         operation = operation_var.get()
 
         if operation == "+":
@@ -24,27 +24,36 @@ def calculate():
 
 # Create the main window
 root = tk.Tk()
-root.title("Simple Calculator")
+root.title("Styled Calculator")
+
+# Styling properties
+root.configure(bg="#f8f9fa")  # Background color
+
+# Function to apply common styles to buttons
+def apply_button_style(widget):
+    widget.config(bg="#007bff", fg="white", font=("Arial", 12), padx=10, pady=5)
 
 # Entry fields
-entry1 = tk.Entry(root)
-entry1.pack()
-entry2 = tk.Entry(root)
-entry2.pack()
+entry1 = tk.Entry(root, font=("Arial", 12))
+entry1.pack(pady=5)
+entry2 = tk.Entry(root, font=("Arial", 12))
+entry2.pack(pady=5)
 
 # Operation selection
 operation_var = tk.StringVar(root)
 operation_var.set("+")
 operations = ["+", "-", "*", "/"]
 operation_dropdown = tk.OptionMenu(root, operation_var, *operations)
-operation_dropdown.pack()
+apply_button_style(operation_dropdown)  # Apply button style
+operation_dropdown.pack(pady=5)
 
 # Calculate button
 calculate_button = tk.Button(root, text="Calculate", command=calculate)
-calculate_button.pack()
+apply_button_style(calculate_button)  # Apply button style
+calculate_button.pack(pady=10)
 
 # Result label
-result_label = tk.Label(root, text="Result: ")
-result_label.pack()
+result_label = tk.Label(root, text="Result: ", bg="#f8f9fa", font=("Arial", 12))
+result_label.pack(pady=5)
 
 root.mainloop()
